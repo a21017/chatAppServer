cff497f13e92647db6bbc0d693e9b0fb446e6405
a21017 <ahmad.amaan1017@gmail.com>
2023-10-31 15:14:21 +0530

    Chat App Server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const userRoute = require('./Routes/userRoute'); 
+const chatRoute = require('./Routes/chatRoute'); 
+const messageRoute = require('./Routes/messageRoute'); 
+app.use(express.json()); //allow send and receive json data
+app.use(cors());
+app.use("/api/users",userRoute);
+app.use("/api/chats",chatRoute);
+app.use("/api/messages",messageRoute);
+//CRUD
+app.get("/", (req,res) => {
+    res.send("Welcome our chat app API...");
+    console.log("received");
+});
+
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req,res)=>{
+    console.log(`Server Running on port ${port}`);
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("MongoDB connection established");
+}).catch((error)=>{console.log("Connection failed ",error.message);})
